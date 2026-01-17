4076ad54b1aeaaee0c67cacd8c2b959ec898be1d
sumitjaiswal55 <sj0855530@gmail.com>
2026-01-17 12:35:01 +0530

    Comments updated in the App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,49 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
-// --- Components ---
-import ProtectedRoute from "./component/ProtectedRoute"; // Jo abhi banaya
-
-// --- Pages ---
+import ProtectedRoute from "./component/ProtectedRoute";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
 import HomeMain from "./pages/home/HomeMain";
-
-// --- Dashboard & Sub-pages ---
-import DashboardLayout from "./pages/farmer/Dashboard"; // Dashboard.jsx (Layout)
+import DashboardLayout from "./pages/farmer/Dashboard";
 import FarmerHome from "./pages/farmer/Home";
 import AddListing from "./pages/farmer/AddListing";
 import MyListing from "./pages/farmer/MyListing";
 import Transaction from "./pages/farmer/Transaction";
 import Orders from "./pages/farmer/Orders";
 import BuyerDashboard from "./pages/buyer/BuyerDashboard";
-import ProductDetails from "./pages/buyer/Products"
-import Profile from "./pages/buyer/Profile"
-import Cart from './pages/buyer/Cart';
+import ProductDetails from "./pages/buyer/Products";
+import Profile from "./pages/buyer/Profile";
+import Cart from "./pages/buyer/Cart";
 
 function App() {
   return (
     <>
       <div>
         <Routes>
-          {/* --- Public Routes (Koi bhi khol sakta hai) --- */}
           <Route path="/" element={<HomeMain />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
 
-          {/* --- Protected Dashboard Routes --- */}
-          <Route 
-            path="/dashboard" 
+          <Route
+            path="/dashboard"
             element={
               <ProtectedRoute>
                 <DashboardLayout />
               </ProtectedRoute>
-            }>
-            <Route index element={<FarmerHome />} /> 
+            }
+          >
+            <Route index element={<FarmerHome />} />
             <Route path="add-listing" element={<AddListing />} />
             <Route path="my-listings" element={<MyListing />} />
             <Route path="transactions" element={<Transaction />} />
             <Route path="orders" element={<Orders />} />
           </Route>
-            <Route path="product" element={<BuyerDashboard />} />
-            <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="product" element={<BuyerDashboard />} />
+          <Route path="product/:id" element={<ProductDetails />} />
 
-            <Route path="profile" element={<Profile />} />
-            <Route path="cart" element={<Cart />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="cart" element={<Cart />} />
         </Routes>
       </div>
     </>
