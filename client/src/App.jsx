e9ccad0a4228fbb52d4e0257712ef6e0d7006067
sumitjaiswal55 <sj0855530@gmail.com>
2026-01-19 10:57:18 +0530

    Changes done into the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,6 @@ function App() {
           <Route path="/" element={<HomeMain />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-
           <Route
             path="/dashboard"
             element={
